e8776bca14c82a1142dac26e9dfb6269e81b91d1
lqh1106 <lqh031106@mail.ustc.edu.cn>
2024-06-05 14:49:29 +0800

    6/5 1.3.1 支持面向明文直接加密

--- scrambling.py
+++ scrambling.py
@@ -1,39 +1,36 @@
 from numpy import mod
-from math import pi,cos,acos,sin
+import sys
+from math import pi, cos, acos, sin
 
 
-def logistic(u, x0, m, n):
-	if 3.57 <= u <= 4:
-		x = x0
-		for i in range(m):
-			x = u * x * (1 - x)
-		scramble = [x]
-		for i in range(n):
-			scramble.append(u * scramble[-1] * (1 - scramble[-1]))
-		return sort(scramble)
-	else:
-		print("Please choose u in range[3.57,4]")
-
-
-def circle(a, b, x0, m, n):
+def logistic(x0, m, n):
 	x = x0
 	for i in range(m):
-		x = mod(x + b - a / (2 * pi) * sin(2 * pi * x), 1)
+		x = 3.60 * x * (1 - x)
 	scramble = [x]
-	for i in range(n):
-		scramble.append(mod(scramble[-1] + b - a / (2 * pi) * sin(2 * pi * scramble[-1]), 1))
+	for i in range(n - 1):
+		scramble.append(3.60 * scramble[-1] * (1 - scramble[-1]))
 	return sort(scramble)
 
 
-def chebyshev(a, x0, m, n):
+def circle(x0, m, n):
 	x = x0
 	for i in range(m):
-		x = cos(a * acos(x))
+		x = mod(x + 0.2 - 0.5 / (2 * pi) * sin(2 * pi * x), 1)
 	scramble = [x]
-	for i in range(n):
-		scramble.append(cos(a * acos(scramble[-1])))
+	for i in range(n - 1):
+		scramble.append(mod(scramble[-1] + 0.2 - 0.5 / (2 * pi) * sin(2 * pi * scramble[-1]), 1))
 	return sort(scramble)
 
+
+def chebyshev(x0, m, n):
+	x = x0
+	for i in range(m):
+		x = cos(4 * acos(x))
+	scramble = [x]
+	for i in range(n - 1):
+		scramble.append(cos(4 * acos(scramble[-1])))
+	return sort(scramble)
 
 
 def sort(scrambling):
@@ -43,3 +40,50 @@ def sort(scrambling):
 		disorganizedtable.append(sortedlist.index(i))
 	# print(disorganizedtable)
 	return disorganizedtable
+
+
+def decode(c, way, k):
+	m_list = [0] * len(c)
+	if way == 0:
+		disorganizedtable = logistic(k, 1000, len(c))
+	elif way == 1:
+		disorganizedtable = circle(k, 1000, len(c))
+	elif way == 2:
+		disorganizedtable = chebyshev(k, 1000, len(c))
+	else:
+		print("invalid way")
+
+	dedisorganizedtable = [0] * len(disorganizedtable)
+
+	for i in range(0, len(disorganizedtable)):
+		dedisorganizedtable[i] = disorganizedtable.index(i)
+
+	for i in range(0, len(dedisorganizedtable)):
+		m_list[dedisorganizedtable[i]] = c[i]
+	m = ''.join(m_list)
+	return m
+
+
+def encode(m, way, k):
+	c_list = [0] * len(m)
+	if way == 0:
+		disorganizedtable = logistic(k, 1000, len(m))
+	elif way == 1:
+		disorganizedtable = circle(k, 1000, len(m))
+	elif way == 2:
+		disorganizedtable = chebyshev(k, 1000, len(m))
+	else:
+		print("invalid way")
+	for i in range(0, len(disorganizedtable)):
+		c_list[disorganizedtable[i]] = m[i]
+	c = ''.join(c_list)
+	return c
+
+
+if __name__ == "__main__":
+	m = input("enter your message: ")
+	k = float(input("enter your key"))
+	way = int(input("choose your way:(0-2)"))
+	c = encode(m, way, k)
+	print('Here is your cipher text: ' + c)
+	print(decode(c, way, k))
