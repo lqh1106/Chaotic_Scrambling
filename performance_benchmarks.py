756f7fe995d1483b1da6e8e8d7f7c3a73dabc969
lqh1106 <lqh031106@mail.ustc.edu.cn>
2024-06-06 14:12:23 +0800

    6/6 2.3.2 更改了绘图参数，优化了绘图文件管理

--- performance_benchmarks.py
+++ performance_benchmarks.py
@@ -12,8 +12,9 @@ ypoints3 = []
 tpoints1 = []
 tpoints2 = []
 tpoints3 = []
-k = 10000
-for n in range(50, 100):
+test_count = 100
+test_range = range(50, 100)
+for n in test_range:
 	xpoints.append(n)
 	time_total1 = 0
 	time_total2 = 0
@@ -21,38 +22,38 @@ for n in range(50, 100):
 	order_total1 = 0
 	order_total2 = 0
 	order_total3 = 0
-	for i in range(k):
+	for i in range(test_count):
 		x0 = random.uniform(0.2, 0.8)
 		start_time = time.time()
 		disorganized_table = scrambling.logistic(x0, 1000, n)
 		end_time = time.time()
 		time_total1 += (end_time - start_time)
 		order_total1 += test.test(disorganized_table)
-	ypoints1.append(order_total1 / (k * n))
-	tpoints1.append(time_total1 / k)
+	ypoints1.append(order_total1 / (test_count * n))
+	tpoints1.append(time_total1 / test_count)
 
-	for i in range(k):
+	for i in range(test_count):
 		x0 = random.uniform(0.2, 0.8)
 		start_time = time.time()
 		disorganized_table = scrambling.circle(x0, 1000, n)
 		end_time = time.time()
 		time_total2 += (end_time - start_time)
 		order_total2 += test.test(disorganized_table)
-	ypoints2.append(order_total2 / (k * n))
-	tpoints2.append(time_total2 / k)
+	ypoints2.append(order_total2 / (test_count * n))
+	tpoints2.append(time_total2 / test_count)
 
-	for i in range(k):
+	for i in range(test_count):
 		x0 = random.uniform(0.2, 0.8)
 		start_time = time.time()
 		disorganized_table = scrambling.chebyshev(x0, 1000, n)
 		end_time = time.time()
 		time_total3 += (end_time - start_time)
 		order_total3 += test.test(disorganized_table)
-	ypoints3.append(order_total3 / (k * n))
-	tpoints3.append(time_total3 / k)
+	ypoints3.append(order_total3 / (test_count * n))
+	tpoints3.append(time_total3 / test_count)
 
 	print(n, end=' ')
-plot.plot(xpoints, ypoints1, 'Logistic', ypoints2, 'Circle', ypoints3, 'Chebyshev', 'order_ave', filename='order_ave-N',
-          title='order_ave-N')
-plot.plot(xpoints, tpoints1, 'Logistic', tpoints2, 'Circle', tpoints3, 'Chebyshev', 'time_ave', filename='time_ave-N',
-          title='time_ave-N')
+plot.plot(xpoints, ypoints1, 'Logistic', ypoints2, 'Circle', ypoints3, 'Chebyshev', 'order_ave', 'order_ave-N',
+          'order_ave-N', test_count, test_range)
+plot.plot(xpoints, tpoints1, 'Logistic', tpoints2, 'Circle', tpoints3, 'Chebyshev', 'time_ave', 'time_ave-N',
+          'time_ave-N', test_count, test_range)
--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import os
 
-def plot(xpoints, ypoints1, name1, ypoints2, name2, ypoints3, name3, ylable, filename, title):
+
+def plot(xpoints, ypoints1, name1, ypoints2, name2, ypoints3, name3, ylable, filename, title, test_count, test_range):
 	plt.figure()
 	plt.plot(xpoints, ypoints1, marker='o', label=name1)
 	plt.plot(xpoints, ypoints2, marker='s', label=name2)
@@ -9,4 +11,6 @@ def plot(xpoints, ypoints1, name1, ypoints2, name2, ypoints3, name3, ylable, fil
 	plt.ylabel(ylable)
 	plt.title(title)
 	plt.legend()
-	plt.savefig('plot/' + filename)
+	folder_path = "./plot/test_count=" + str(test_count) + ' in ' + str(test_range)
+	os.makedirs(folder_path, exist_ok=True)
+	plt.savefig(folder_path + '/' + filename)
